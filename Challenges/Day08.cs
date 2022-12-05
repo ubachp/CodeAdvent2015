399ef90f6b0a189e39f330c3d9165815d38562c7
Paulo Ubach <paulo.ubach@gmail.com>
2022-12-04 22:26:31 -0500

    Day 8 challenge preparation

---
+++ Challenges/Day08.cs
@@ -0,0 +1,27 @@
+﻿using CodeAdvent2015.Shared;
+
+namespace CodeAdvent2015.Challenges
+{
+    public class Day08 : AdventDayBase, IAdventDay
+    {
+
+        public Day08(string cookie) : base(cookie)
+        {
+        }
+
+        public async Task Solve()
+        {
+            string input = await Helper.GetInput(@"https://adventofcode.com/2015/day/7/input", Cookie);
+
+            var inputArray = input.Split("\n");
+
+            foreach (var line in inputArray)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                
+            }
+
+            Helper.PrintResults("wire a", 0, "wire a", 0);
+        }
+    }
+}
